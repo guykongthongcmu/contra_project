83b2ddf5ab7a99aadc57574df73df0c1554eacd1
guykongthong <guykongthongcmu@gmail.com>
2025-11-03 08:11:10 +0700

    implemented executable jar

---
+++ src/main/java/se233/contra_project/JarLauncher.java
@@ -0,0 +1,9 @@
+package se233.contra_project;
+
+import se233.contra_project.Launcher;
+
+public class JarLauncher {
+    public static void main(String... args) {
+        Launcher.main(args);
+    }
+}
